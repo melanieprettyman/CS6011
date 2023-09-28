da15828f5b5e64737d24494988882d6c250bbc3a
Melanie <u1060134@umail.utah.edu>
2023-09-28 08:58:33 -0600

    changed main

--- Week1/Day3/Rainfall/src/Main.java
+++ Week1/Day3/Rainfall/src/Main.java
@@ -6,25 +6,55 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    //Function to find the average rainfall for a given month
-    //input array and month
-    static double getAverageRainfallForAMonth(ArrayList<RainfallData> arrayOfData, String month) {
+    /**
+     * Calculates the average rainfall for a specific month from a given list of rainfall data.
+     *dataArray -> An ArrayList of RainfallData objects containing the rainfall data.
+     *month->The month for which the average rainfall is to be calculated.
+     *The average rainfall amount (in inches) for the specified month.
+     */
+    public static double getAverageRainfallForAMonth(ArrayList<RainfallData> dataArray, String month) {
         double totalRainfall = 0.0;
         int monthCounter = 0;
 
-        //Loop through array
-        for (int i = 0; i < arrayOfData.size(); i++) {
-            //If the month equals the month inputted, get the rainfall for that struct in the array
-            //and add it to the totalRainfall
-            //and add 1 to month counter
-            if (arrayOfData.get(i).getMonth().equals(month)) {
-                totalRainfall += arrayOfData.get(i).getRainfall();
-                monthCounter++;
+        // Loop through the list of rainfall data
+        for (RainfallData data : dataArray) {
+            // Check if the month of the current data matches the specified month
+            if (data.getMonth().equals(month)) {
+                totalRainfall += data.getRainfall(); // Add the rainfall amount for this data point
+                monthCounter++; // Increment the counter for this month
             }
         }
-        //return average
-        return totalRainfall / monthCounter;
+        // Calculate and return the average rainfall for the specified month
+            return totalRainfall / monthCounter;
     }
+
+    /**
+     * Writes the average rainfall for each month to a specified output file.
+     * outputPath -> The path to the output file where the results will be written.
+     * dataArray->An ArrayList of RainfallData objects containing the rainfall data.
+     */
+    public static void writeAverageRainfallToFile(String outputPath, ArrayList<RainfallData> dataArray) {
+        try {
+            FileWriter writer = new FileWriter(outputPath);
+
+            // Array of month names
+            String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+            // Iterate through each month and calculate the average rainfall, then write it to the file
+            for (String month : months) {
+                double average = getAverageRainfallForAMonth(dataArray, month);
+                writer.write("Average rainfall for " + month + ": " + average + " inches.\n");
+            }
+
+            writer.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
+
+
 
     public static void main(String[] args) throws IOException {
 
@@ -32,60 +62,17 @@ public class Main {
         String filepath = "/Users/melanieprettyman/Desktop/MSD/CS6011/CS6011/Week1/Day3/Rainfall/src/Atlanta.txt"; // Open file
 
         //Create a new object, reads in data
-        RainfallData alantaData = new RainfallData(filepath);
+        RainfallData atlantaData = new RainfallData(filepath);
 
-        //Create an array of data structs 
-        ArrayList<RainfallData> dataList = alantaData.ReadFile();
+        //Create an array of data structs
+        ArrayList<RainfallData> dataList = atlantaData.ReadFile();
 
-        //Call function that gets the average rainfall for a particular month.
-        double avgerage_January = getAverageRainfallForAMonth(dataList, "January");
-        double avgerage_February = getAverageRainfallForAMonth(dataList, "February");
-        double avgerage_March = getAverageRainfallForAMonth(dataList, "March");
-        double avgerage_April = getAverageRainfallForAMonth(dataList, "April");
-        double avgerage_May = getAverageRainfallForAMonth(dataList, "May");
-        double avgerage_June = getAverageRainfallForAMonth(dataList, "June");
-        double avgerage_July = getAverageRainfallForAMonth(dataList, "July");
-        double avgerage_August = getAverageRainfallForAMonth(dataList, "August");
-        double avgerage_September = getAverageRainfallForAMonth(dataList, "September");
-        double avgerage_October = getAverageRainfallForAMonth(dataList, "October");
-        double avgerage_November = getAverageRainfallForAMonth(dataList, "November");
-        double avgerage_December = getAverageRainfallForAMonth(dataList, "December");
+        //Create file path for outputted file
+        String outputPath = "/Users/melanieprettyman/Desktop/MSD/CS6011/CS6011/Week1/Day3/Rainfall/src/rainfall_results.txt"; // Open file
 
+        //wrtie to outputted file
+        writeAverageRainfallToFile(outputPath, dataList);
 
-
-        //PROGRAM OUTPUT
-
-        //file name with path
-        String filePath = "/Users/melanieprettyman/Desktop/MSD/CS6011/CS6011/Week1/Day3/Rainfall/src/rainfall_results.txt"; // Open file
-
-
-        //write in file made above month averages
-        try {
-            FileWriter myWriter = new FileWriter(filePath);
-            myWriter.write("Average rainfall for January: " + avgerage_January + " inches.\n");
-            myWriter.write("Average rainfall for February: " + avgerage_February + " inches.\n");
-            myWriter.write("Average rainfall for March: " + avgerage_March + " inches.\n");
-            myWriter.write("Average rainfall for April: " + avgerage_April + " inches.\n");
-            myWriter.write("Average rainfall for May: " + avgerage_May + " inches.\n");
-            myWriter.write("Average rainfall for June: " + avgerage_June + " inches.\n");
-            myWriter.write("Average rainfall for July: " + avgerage_July + " inches.\n");
-            myWriter.write("Average rainfall for August: " + avgerage_August + " inches.\n");
-            myWriter.write("Average rainfall for September: " + avgerage_September + " inches.\n");
-            myWriter.write("Average rainfall for October: " + avgerage_October + " inches.\n");
-            myWriter.write("Average rainfall for November: " + avgerage_November + " inches.\n");
-            myWriter.write("Average rainfall for December: " + avgerage_December + " inches.\n");
-
-            //close the file
-            myWriter.close();
-
-            //print a success message if this all happened
-            System.out.println("Successfully wrote to the file.");
-
-            //if the program wasn't able to write out to the file then print an error message
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
     }
 
 }
